d51e9976db342eaee9d8a5342bfd1b64ebb62c5b
Rico Beti <rico.beti@silentbyte.com>
2017-02-03 23:20:59 +0800

    Added fixed size integer types.

--- sb/sb_memory/include/sb/defs.hpp
+++ sb/sb_memory/include/sb/defs.hpp
@@ -26,9 +26,20 @@
 #   define SB_MEMORY_DEFS_HPP
 
 #include <cstddef>
+#include <cstdint>
 
 namespace sb
 {
+    using u8 = std::uint8_t;
+    using u16 = std::uint16_t;
+    using u32 = std::uint32_t;
+    using u64 = std::uint64_t;
+
+    using i8 = std::int8_t;
+    using i16 = std::int16_t;
+    using i32 = std::int32_t;
+    using i64 = std::int64_t;
+
     constexpr std::size_t default_alignment = sizeof(std::size_t);
 }
 
